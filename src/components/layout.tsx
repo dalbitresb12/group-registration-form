dece1df7e3fb30dd7c2de18c97d26f5ebcdf605b
Diego Albitres <diego.albitres@gmail.com>
2021-06-04 12:39:44 -0500

    Added padding option to layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,22 +1,24 @@
+import clsx from 'clsx';
 import Head from 'next/head';
 import { Footer } from './footer';
 
 export type Props = {
   title: string,
+  padding?: boolean,
   children: React.ReactNode
 };
 
 export const Layout = (props: Props): React.ReactElement => {
-  const { title, children } = props;
+  const { title, padding = true, children } = props;
 
   return (
-    <div className="min-h-screen px-4 lg:px-2 flex flex-col justify-center items-center">
+    <div className={clsx("min-h-screen flex flex-col justify-center items-center", padding && "px-4 lg:px-2")}>
       <Head>
         <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="py-20 flex flex-1 flex-col justify-center items-center">
+      <main className={clsx(padding && "py-20", "flex flex-1 flex-col justify-center items-center")}>
         {children}
       </main>
 
